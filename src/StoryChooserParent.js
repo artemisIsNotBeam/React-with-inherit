c9065a21f4695eda604fce2acef2fcc0222bdbb8
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-03-20 14:43:55 -0700

    Finally finished and deleted unesscary stuff

--- src/StoryChooserParent.js
+++ src/StoryChooserParent.js
@@ -2,39 +2,41 @@ import './App.css';
 import React from 'react';
 import Buttan from './storychooser';
 
-const example = {
-    repl:["replt","repltUserName","repltpassword"],
-    github:["github","githubPassword","githubPossword"]
+const Passwords = {
+    repl:["repltUserName","repltpassword"],
+    github:["githubPassword","githubPossword"]
   }
 
 class ParentStory extends React.Component {
     constructor(props){
       super(props);
       this.state = { 
-        Index:1, 
-        story:example, 
-        message:"this run first",
-        prompt:"passwowadsar",
+        pwdList:Passwords, 
+        password:"password",
         userName:"username",
-        option:example.repl[0],
-        option2:example.github[0]
+        option:Passwords.repl[0],
+        option2:Passwords.github[0]
       }
-
+      // let Increment Index change state
       this.IncrementIndex = this.IncrementIndex.bind(this);
     }
-
+    // find new state
     IncrementIndex(option){
+      //option returns a list
       this.setState({
-        prompt:option[2],
-        userName:option[1]
+        password:option[1],
+        userName:option[0]
       })
     }
+
     render(){
         return (
         <div>
             <h1>Password</h1>
-            <Buttan update={this.IncrementIndex} daStory={this.state.story} Index={this.state.Index} 
-            prompt={this.state.userName} password={this.state.prompt}option={this.state.option} option2={this.state.option2}/>
+            {/* both options are what to render in child */}
+            <Buttan update={this.IncrementIndex} PasswordList={this.state.pwdList}
+            userName={this.state.userName} password={this.state.password} 
+            option={this.state.option} option2={this.state.option2}/>
         </div>
         )
     }
--- src/storychooser.js
+++ src/storychooser.js
@@ -7,7 +7,7 @@ class Buttan extends React.Component {
     super(props);
     this.upDate= this.upDate.bind(this);
   }
-
+  //run  the props to keep <button> tags length down
   upDate(option){
     this.props.update(option);
   }
@@ -15,10 +15,12 @@ class Buttan extends React.Component {
   render() {
     return (
       <div>
-          <p id="username">{this.props.prompt}</p>
+          {/* top two are username and password */}
+          <p id="username">{this.props.userName}</p>
           <p id="password">{this.props.password}</p>
-          <button id="button1" onClick={() =>{this.upDate(this.props.daStory.repl)} }> {this.props.option}</button>
-          <button id="button2" onClick={() =>{this.upDate(this.props.daStory.github)} }> {this.props.option2}</button>
+          {/* () => to make sure it only runs when clicked otherwise it runs when website */}
+          <button id="button1" onClick={() =>{this.upDate(this.props.PasswordList.repl)} }> {this.props.option}</button>
+          <button id="button2" onClick={() =>{this.upDate(this.props.PasswordList.github)} }> {this.props.option2}</button>
       </div>
     )
   }
